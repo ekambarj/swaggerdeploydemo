d5135b0af22e3b29b393e98e9455b7fb0d29fb31
Manoj N V <manoj_nv@thbs.com>
2013-02-01 12:43:03 +0000

    Changes to files which gives a custom URL for the resource listing, but fails with internal error

--- src/main/java/uk/co/o2/customer/ApiListingResource.java
+++ src/main/java/uk/co/o2/customer/ApiListingResource.java
@@ -6,9 +6,10 @@ package uk.co.o2.customer;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import com.wordnik.swagger.annotations.*;
 import com.wordnik.swagger.jaxrs.JavaApiListing;
 
-@Path("/resources.json")
-//@Api("/resources")
+@Path("/resources")
+@Api("/resources")
 @Produces({"application/json"})
 public class ApiListingResource extends JavaApiListing {}
